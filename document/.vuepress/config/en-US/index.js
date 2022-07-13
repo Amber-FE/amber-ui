f27fd5e282c960ae5441b998c202ed374201bcdc
Liboq <99778162+Liboq@users.noreply.github.com>
2022-07-13 09:51:51 +0800

    ci(other): 修改多语言配置

---
+++ document/.vuepress/config/en-US/index.js
@@ -0,0 +1,6 @@
+const { NavItems4EN } = require('./nav')
+const { Sidebar4EN } = require('./sidebar')
+module.exports = {
+  NavItems4EN,
+  Sidebar4EN
+}
---
+++ document/.vuepress/config/zh-CN/index.js
@@ -0,0 +1,6 @@
+const { NavItems4ZH } = require('./nav')
+const { Sidebar4ZH } = require('./sidebar')
+module.exports={
+  NavItems4ZH,
+  Sidebar4ZH
+}
